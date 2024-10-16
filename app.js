1e6da7dc4ff993fa1751a2562ec1d37f98ad4141
chamer79 <chamer79@gmail.com>
2024-10-16 17:49:44 -0400

    completed exercise 3

--- app.js
+++ app.js
@@ -23,3 +23,19 @@ const [firstTopping, secondTopping] = pizzaToppings
 
 console.log(firstTopping)
 console.log(secondTopping)
+
+
+// Exercise 3: Destructuring objects
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+const car = {
+    make: 'Audi',
+    model: 'q5',
+  };
+  
+  // Your code here
+  const { make, model } = car
+
+  console.log(make)
+  console.log(model)
+  
