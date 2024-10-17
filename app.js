eade9c245f8f7d639053732ccd07f8d3a84f9b58
chamer79 <chamer79@gmail.com>
2024-10-16 20:07:34 -0400

    completed exercise 8

--- app.js
+++ app.js
@@ -106,9 +106,23 @@ console.log(userProfile)
         // import name, { age, job } from './exportingFile.js';
         // console.log(name, age, job);
     
-    
 
-    
+// Exercise 8: Default parameters
+// Create a function that takes two parameters, `noun` and `adjective`, both with the following respective default values:
+
+// 1. `cat`
+
+// 2. `white`
+
+// The function should log a sentence 'The cat is white.' by default. The function should substitute the appropriate parameters when supplied arguments.
+
+// Your code here
+const madLib = (noun = "cat", adjective = "white") => {
+    return (`The ${noun} is ${adjective}.`)
+}
+
+console.log("Default values: ",madLib())
+console.log("Passed in values: ", madLib("dog", "smelly"))
 
     
 
